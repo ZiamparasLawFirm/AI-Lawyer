5395ff2a0a706be3c6e7a42842711aabbfd96727
dimitrisziamparas <dimitris.ziamparas@yahoo.com>
2025-08-03 22:12:39 +0300

    Initial Commit

--- Main.py
+++ Main.py
@@ -1,8 +1,25 @@
-print("Hi, I am your lawyer, how can I help you?")
+from dotenv import load_dotenv
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-while True:
-    user_input = input("You: ")
-    if user_input == "exit":
-        break
-    print(f"Cool, thanks for sharing that {user_input}")
+load_dotenv()
+
+gemini_key = os.getenv("GEMINI_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=gemini_key,
+    temperature=0.5
+)
+
+response = llm.invoke([{"role":"user", "content":"Hi there, how are you?"}])
+print(response.content)
+
+#print("Hi, I am your lawyer, how can I help you?")
+
+#while True:
+#    user_input = input("You: ")
+#    if user_input == "exit":
+#        break
+#    print(f"Cool, thanks for sharing that {user_input}")
 
