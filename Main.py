4c7befac8bbada523ed4b4575a951bd726a3c6e7
dimitrisziamparas <dimitris.ziamparas@yahoo.com>
2025-08-03 21:18:03 +0300

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,8 @@
+print("Hi, I am your lawyer, how can I help you?")
+
+while True:
+    user_input = input("You: ")
+    if user_input == "exit":
+        break
+    print(f"Cool, thanks for sharing that {user_input}")
+
