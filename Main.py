3c094554d2846e483aab06cf10888b3a55a5050f
dimitrisziamparas <dimitris.ziamparas@yahoo.com>
2025-08-03 22:23:39 +0300

    Initial Commit

--- Main.py
+++ Main.py
@@ -6,13 +6,19 @@ load_dotenv()
 
 gemini_key = os.getenv("GEMINI_API_KEY")
 
+system_prompt = """
+You are a Greek Lawyer
+"""
+
+user_input = input()
+
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     google_api_key=gemini_key,
     temperature=0.5
 )
 
-response = llm.invoke([{"role":"user", "content":"Hi there, how are you?"}])
+response = llm.invoke([{"role":"user", "content":system_prompt+user_input}])
 print(response.content)
 
 #print("Hi, I am your lawyer, how can I help you?")
