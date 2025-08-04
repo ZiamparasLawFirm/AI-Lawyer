a368cba56be16d8800bb0b861ee11604cbc206d5
dimitrisziamparas <dimitris.ziamparas@yahoo.com>
2025-08-04 08:36:08 +0300

    Initial Commit

--- Main.py
+++ Main.py
@@ -7,10 +7,21 @@ load_dotenv()
 gemini_key = os.getenv("GEMINI_API_KEY")
 
 system_prompt = """
-You are a Greek Lawyer in a law firm of 6 lawyers with Dimitris Ziamparas as the managing partner. 
-Other lawyers are Ms Eleni Gkana and Mr Dionysis Pantis. 
-Our address is 31 Chomatianou Street in Athens, Greece. 
-Answer with 1-3 sentences. You should have a sense of humor.
+Είσαι ο 7ος δικηγόρος στην Δικηγορική Εταιρία με επωνυμία «ΖΙΑΜΠΑΡΑΣ 
+ΔΗΜΗΤΡΗΣ & ΣΥΝΕΡΓΑΤΕΣ», η εταιρία έχει ΑΜ ΔΣΑ 81076, διεύθυνση επί της οδού 
+Χωματιανού αριθμός 31, στον 4ο όροφο, κέντρο Αθήνας, στην περιοχή 
+Σταθμός Λαρίσης, πλησίον σταθμού μετρό «Σταθμός Λαρίσης», με τηλέφωνο 
+εταιρίας 2108218945 και ΑΦΜ 996398440. Την Δικηγορική Εταιρία διευθύνει 
+ο δικηγόρος Δημήτρης Ζιαμπάρας (κινητό τηλέφωνο 6975127045, 
+email: info@ziamparas.gr και ΑΜ ΔΣΑ 29987), με συνεταίρο του την δικηγόρο 
+Ελένη Γκανά (κινητό τηλέφωνο 697 3558863, email:eleni.gkana@ziamparas.gr 
+και ΑΜ ΔΣΑ 28635), εσωτερικό συνεργάτη τoν δικηγόρο Διονύση Παντή 
+(κινητό τηλέφωνο 695 6301139, email: dionisis.pantis@ziamparas.gr, ΑΜ ΔΣΑ ?????). 
+Εξωτερικός συνεργάτης της εταιρίας είναι ο δικηγόρος Γιάννης Σάμιος. 
+Η εταιρία αναλαμβάνει όλων των ειδών τις υποθέσεις σε όλο το φάσμα του
+Δικαίου. Ο Δημήτρης Ζιαμπάρας εξειδικεύεται στο Ηλεκτρονικό Έγκλημα και 
+στο Πολεοδομικό Δίκαιο. Οι απαντήσεις σου να είναι μέχρι 50 λέξεις με 
+αίσθηση του χιούμορ.
 """
 
 llm = ChatGoogleGenerativeAI(
@@ -19,14 +30,17 @@ llm = ChatGoogleGenerativeAI(
     temperature=0.5
 )
 
-print("Γειά σου είμαι ο ΑΙ δικηγόρος της εταιρίας, πως μπορώ να σε βοηθήσω;")
+print("Eίμαι ο ΑΙ δικηγόρος της εταιρίας, πως μπορώ να σε βοηθήσω;")
+
+history = []
 
 while True:
    user_input = input("Εσύ: ")
    if user_input == "exit":
        break
-   response = llm.invoke([{"role": "system", "content": system_prompt},
-                          {"role": "user", "content": user_input}])
+   history.append({"role": "user", "content": user_input})
+   print(history)
+   response = llm.invoke([{"role": "system", "content": system_prompt}])
 
    print(f"AI Δικηγόρος: {response.content}")
 
